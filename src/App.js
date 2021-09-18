7ab0ab3ff423258f44746a589ca6c9786712c8b8
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2021-09-17 23:16:38 -0700

    changed App class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-import { Cards, Chart, CountryPicker } from './components'
-import styles from './App.module.css'
-import { fetchData } from './api'
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
 
-import coronaImage from './images/image.png'
+import coronaImage from './images/image.png';
 
-class App extends React.Component {
-  state = {
-    data: {},
-    country: ''
-  }
+const App = () => {
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState('');
 
-  async componentDidMount() {
-    const fetchedData = await fetchData()
-    this.setState({ data: fetchedData })
-  }
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      const fetchedData = await fetchData();
+      setData(fetchedData);
+    };
+    fetchDataFromAPI();
+  }, []);
 
-  handleCountryChange = async country => {
-    const fetchedData = await fetchData(country)
-    this.setState({ data: fetchedData, country: country })
-  }
+  const handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    setData(fetchedData);
+    setCountry(country);
+  };
 
-  render() {
-    const { data, country } = this.state
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={coronaImage} alt="COVID-19" />
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart data={data} country={country} />
+    </div>
+  );
+};
 
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={coronaImage} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} />
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
